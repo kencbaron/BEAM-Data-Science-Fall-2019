71fc549b9a96b6303b06b9319717a5799e546fe6
kencbaron <55363228+kencbaron@users.noreply.github.com>
2019-09-29 13:56:59 -0400

    Created using Colaboratory

--- helloWorld.py
+++ helloWorld.py
@@ -3,7 +3,7 @@
   "nbformat_minor": 0,
   "metadata": {
     "colab": {
-      "name": "Copy of Untitled3.ipynb",
+      "name": "Untitled4.ipynb",
       "provenance": [],
       "include_colab_link": true
     },
@@ -26,37 +26,12 @@
     {
       "cell_type": "code",
       "metadata": {
-        "id": "-kiTojKYIX1M",
-        "colab_type": "code",
-        "outputId": "e53dc931-b30e-4e91-fa03-9ca5f530d1cc",
-        "colab": {
-          "base_uri": "https://localhost:8080/",
-          "height": 34
-        }
-      },
-      "source": [
-        "print('Hello world')"
-      ],
-      "execution_count": 0,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "text": [
-            "Hello world\n"
-          ],
-          "name": "stdout"
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "3yMhrkBCIqhB",
+        "id": "DCgHgNohKny7",
         "colab_type": "code",
         "colab": {}
       },
       "source": [
-        ""
+        "print(\"Hello World\")"
       ],
       "execution_count": 0,
       "outputs": []
